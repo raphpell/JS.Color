873dab97d4b1c2658585934160deb33e261c7c30
raphpell <pellicierraphael@neuf.fr>
2021-10-10 10:29:06 +0200

    UnitTest+Compression

--- Color.js
+++ Color.js
@@ -1,114 +1,109 @@
 Color =(function(){
-	let Color = s =>{
-		if( ! s || ! s.charAt ) return s
-		if( s.charAt(0)=='#' || s.length<=6 ) return Color.hex( s )
-		for(let a=['rgb','hsv','hsl','cmyk'], i=0, ni=a.length; i<ni; i++ )
+	let Color= s =>{
+		if(!s||!s.charAt) return s
+		if(s.charAt(0)=='#'||s.length<=6) return Color.hex( s )
+		for(let a=['rgb','hsv','hsl','cmyk'],i=0,ni=a.length;i<ni;i++)
 			if(!s.indexOf(a[i])||!s.indexOf(a[i].toUpperCase()))
-				return Color[a[i]]( s )
-		throw new Error ('INVALID_COLOR '+ s )
+				return Color[a[i]](s)
+		throw new Error ('INVALID_COLOR '+s)
 		}
-
-	, MAX= {r:255,g:255,b:255,h:359,s:100,v:100,l:100,c:100,m:100,y:100,k:100,a:1}
-
-	// VALIDATIONS
-	, parseString =( s, sMode )=>{
-		let a =[], i =-1, re =new RegExp( "^\\s*"+ sMode +"?\\s*\\(([^)]+)\\)", 'gim' )
-		s.replace( re, function( sFound, $1 ){
-			$1.replace( /\s*(-?\.?\d+(?:\.\d*)?)(%?)\s*,?/gim, function( sFound, $1, $2 ){
-				if( i++ < 4 ) a[i] = $2 ? MAX[ sMode.charAt(i)] * $1 / 100 : $1*1
+	, MAX={r:255,g:255,b:255,h:359,s:100,v:100,l:100,c:100,m:100,y:100,k:100,a:1}
+	// Validations
+	, parseString= (s,sMode)=>{
+		let a=[]
+		,i=-1
+		,re=new RegExp("^\\s*"+sMode+"?\\s*\\(([^)]+)\\)", 'gim')
+		s.replace(re,function(sFound,$1){
+			$1.replace(/\s*(-?\.?\d+(?:\.\d*)?)(%?)\s*,?/gim,function(sFound,$1,$2){
+				if(i++<4) a[i]=$2?MAX[ sMode.charAt(i)]*$1/100: $1*1
 				})
 			})
-		if( a.length ) return getObject( sMode, a )
+		if(a.length) return getObject(sMode,a)
 		return null
 		}
-	, getObject =( sMode, a )=>{
+	, getObject= (sMode,a)=>{
 		let o ={}
-		for(let i=0; s=sMode.charAt(i); i++ ) o[s] = inRange( a[i], s )
+		for(let i=0;s=sMode.charAt(i);i++) o[s]=inRange(a[i],s)
 		return o
 		}
-	, inRange =(n,s)=>{
-		if( isNaN(n) || ! MAX[s]) return null
-		return n>MAX[s]?MAX[s]:(n<0?0:n)
+	, inRange= (n,s)=> (isNaN(n)||!MAX[s])?null:(n>MAX[s]?MAX[s]:(n<0?0:n))
+	, isHEX= s =>{
+		if(s[0]=='#')s=s.slice(1)
+		if(s.length==1)s=s[0]+s[0]+s[0]+s[0]+s[0]+s[0]
+		if(s.length==3)s=s[0]+s[0]+s[1]+s[1]+s[2]+s[2]
+		if(/^[\dABCDEF]{6}$/i.test(s)) return s.toUpperCase()
+		throw new Error('INVALID_HEXADECIMAL_COLOR '+s)
 		}
-	, isHEX = s =>{
-		if( s[0]=='#' ) s = s.slice(1)
-		if( s.length==1 ) s = s[0]+s[0]+s[0]+s[0]+s[0]+s[0]
-		if( s.length==3 ) s = s[0]+s[0]+s[1]+s[1]+s[2]+s[2]
-		if( /^[\dABCDEF]{6}$/i.test( s )) return s.toUpperCase()
-		throw new Error ('INVALID_HEXADECIMAL_COLOR '+ s )
-		}
-	, is =( sMode, X, Y, Z, A )=>{
-		let o = isNaN(Y)
-			? parseString( X, sMode )
-			: getObject(sMode, [0+X,0+Y,0+Z,A]) // Fast Conversion
+	, is= (sMode,X,Y,Z,A)=>{
+		let o =isNaN(Y)?parseString(X,sMode):getObject(sMode,[0+X,0+Y,0+Z,A])
 		if( o ){
-			o.mode = o.a===null ? sMode.slice(0,-1) : sMode
+			o.mode=o.a===null?sMode.slice(0,-1):sMode
 			return o
 			}
-		throw new Error ('INVALID_'+ sMode.toUpperCase() +'_COLOR ('+X+','+Y+','+Z+','+A+')' )
+		throw new Error('INVALID_'+sMode.toUpperCase()+'_COLOR ('+X+','+Y+','+Z+','+A+')')
 		}
-
-	// CONVERSIONS
-	, f1= DECtoHEX = n =>{
+	// Conversions
+	, f1=DECtoHEX= n =>{
 		n=n<0?0:(n>255?255:Math.round(n))
 		if(n==0)return'00'
 		n=new Number(n)
 		let s=n.toString(16).toUpperCase()
 		return(s.length==1?'0':'')+s
 		}
-	, f2= HEXtoDEC = s =>{
-		let n=0, f= n => '0123456789ABCDEF'.indexOf( s.charAt(n))
-		for(let nCoef=0, i=s.length-1; i>=0; i--, nCoef++)
+	, f2=HEXtoDEC= s =>{
+		let n=0
+		,f= n => '0123456789ABCDEF'.indexOf(s.charAt(n))
+		for(let nCoef=0,i=s.length-1;i>=0;i--,nCoef++)
 			n+=f(i)*Math.pow(16,nCoef)
 		return n
 		}
-	, HEXtoRGB = HEX =>{
-		let f = n => f2(HEX.substr(n,2))
+	, HEXtoRGB= HEX =>{
+		let f= n => f2(HEX.substr(n,2))
 		return Color.rgb(f(0),f(2),f(4))
 		}
-	, RGBtoHEX = o => Color.hex( f1(o.r) + f1(o.g) + f1(o.b))
-	, RGBtoHSV = o =>{
+	, RGBtoHEX= o => Color.hex(f1(o.r)+f1(o.g)+f1(o.b))
+	, RGBtoHSV= o =>{
 		let r=o.r/MAX.r, g=o.g/MAX.g, b=o.b/MAX.b
-		, max = Math.max(r,g,b), min = Math.min(r,g,b)
-		, h, s, v = max
-		, d = max-min
-		s = max==0?0:d/max
-		if( max==min ) h = 0 // achromatic
+		, max=Math.max(r,g,b), min=Math.min(r,g,b)
+		, h, s, v=max
+		, d=max-min
+		s=max==0?0:d/max
+		if(max==min) h=0 // achromatic
 		else{
-			switch( max ){
-				case r: h = (g-b)/d+( g<b ? 6 : 0 ); break
-				case g: h = (b-r)/d+2; break
-				case b: h = (r-g)/d+4; break
-				}
-			h /= 6
-			}
-		return Color.hsv( h*MAX.h, s*MAX.s, v*MAX.v, o.a )
-		}
-	, RGBtoHSL = o =>{
-		let r=o.r/MAX.r, g=o.g/MAX.g, b=o.b/MAX.b
-		, max = Math.max(r,g,b), min = Math.min(r,g,b)
-		, h, s, l = (max+min)/2
-		if( max==min ) h = s = 0 // achromatic
-		else{
-			let d = max-min
-			s = l>0.5 ? d/(2-max-min) : d/(max+min)
 			switch(max){
-				case r: h = (g-b)/d+(g<b?6:0); break
-				case g: h = (b-r)/d+2; break
-				case b: h = (r-g)/d+4; break
+				case r:h=(g-b)/d+(g<b?6:0); break
+				case g:h=(b-r)/d+2; break
+				case b:h=(r-g)/d+4; break
 				}
-			h /= 6
+			h/=6
+			}
+		return Color.hsv(h*MAX.h,s*MAX.s,v*MAX.v,o.a)
+		}
+	, RGBtoHSL= o =>{
+		let r=o.r/MAX.r, g=o.g/MAX.g, b=o.b/MAX.b
+		, max=Math.max(r,g,b), min=Math.min(r,g,b)
+		, h, s, l=(max+min)/2
+		if(max==min) h=s=0 // achromatic
+		else{
+			let d=max-min
+			s = l>0.5?d/(2-max-min):d/(max+min)
+			switch(max){
+				case r: h=(g-b)/d+(g<b?6:0); break
+				case g: h=(b-r)/d+2; break
+				case b: h=(r-g)/d+4; break
+				}
+			h/=6
 			}
 		return Color.hsl( h*MAX.h, s*MAX.s, l*MAX.l, o.a )
 		}
-	, HSVtoRGB = o =>{
+	, HSVtoRGB= o =>{
 		let h=o.h/MAX.h, s=o.s/MAX.s, v=o.v/MAX.v
 		, r, g, b
-		, i = Math.floor(h*6)
-		, f = h*6-i
-		, p = v*(1-s)
-		, q = v*(1-f*s)
-		, t = v*(1-(1-f)*s)
+		, i=Math.floor(h*6)
+		, f=h*6-i
+		, p=v*(1-s)
+		, q=v*(1-f*s)
+		, t=v*(1-(1-f)*s)
 		switch( i%6 ){
 			case 0: r=v,g=t,b=p; break
 			case 1: r=q,g=v,b=p; break
@@ -117,24 +112,23 @@ Color =(function(){
 			case 4: r=t,g=p,b=v; break
 			case 5: r=v,g=p,b=q; break
 			}
-		return Color.rgb( r*MAX.r, g*MAX.g, b*MAX.b, o.a )
+		return Color.rgb(r*MAX.r,g*MAX.g,b*MAX.b,o.a)
 		}
-	, HSVtoHSL = o =>{
-		let L = (2-o.s/100)*o.v/2
-		, S = o.s*o.v/(L<50?L*2:200-L*2)
-		if( isNaN(S)) S = 0
+	, HSVtoHSL= o =>{
+		let L=(2-o.s/100)*o.v/2, S=o.s*o.v/(L<50?L*2:200-L*2)
+		if(isNaN(S)) S=0
 		return Color.hsl(o.h,S,L,o.a)
 		}
-	, HSLtoRGB = o =>{
+	, HSLtoRGB= o =>{
 		let h=o.h/MAX.h
 		, s=o.s/MAX.s
 		, l=o.l/MAX.l
 		, r,g,b
-		if( s==0 ) r=g=b=l // achromatic
+		if(s==0) r=g=b=l // achromatic
 		else{
 			let q = l<0.5?l*(1+s):l+s-l*s
-			, p = 2*l-q
-			, hue2rgb = t =>{
+			, p=2*l-q
+			, hue2rgb= t =>{
 				if(t<0) t+=1
 				if(t>1) t-=1
 				if(t<1/6) return p+(q-p)*6*t
@@ -142,55 +136,54 @@ Color =(function(){
 				if(t<2/3) return p+(q-p)*(2/3-t)*6
 				return p
 				}
-			r = hue2rgb(h+1/3)
-			g = hue2rgb(h)
-			b = hue2rgb(h-1/3)
+			r=hue2rgb(h+1/3)
+			g=hue2rgb(h)
+			b=hue2rgb(h-1/3)
 			}
 		return Color.rgb(r*MAX.r,g*MAX.g,b*MAX.b,o.a)
 		}
-	, HSLtoHSV = o =>{
-		let a = 2*o.l/MAX.l
-		, b = (a<=1?a:(2-a))*o.s/MAX.s
-		, S = a+b==0?0:(2*b)/(a+b)*MAX.s
+	, HSLtoHSV= o =>{
+		let a=2*o.l/MAX.l
+		, b=(a<=1?a:(2-a))*o.s/MAX.s
+		, S=a+b==0?0:(2*b)/(a+b)*MAX.s
 		return Color.hsv(o.h,S,(a+b)/2*MAX.v,o.a)
 		}
-	, CMYKtoRGB = o =>{
-		let n = 255*(100-o.k)/10000
-		, f = s => n*(100-o[s])
+	, CMYKtoRGB= o =>{
+		let n=255*(100-o.k)/10000
+		, f= s => n*(100-o[s])
 		return Color.rgb(f('c'),f('m'),f('y'))
 		}
-	, RGBtoCMYK = o =>{
-		let r= o.r/MAX.r
-		, g= o.g/MAX.g, b= o.b/MAX.b
-		, k = 1-Math.max(r,g,b)
-		, f = N => k==1?0:MAX.c*(1-N-k)/(1-k)
-		return Color.cmyk( f(r), f(g), f(b), k*MAX.k )
+	, RGBtoCMYK= o =>{
+		let r=o.r/MAX.r, g=o.g/MAX.g, b=o.b/MAX.b
+		, k=1-Math.max(r,g,b)
+		, f= N => k==1?0:MAX.c*(1-N-k)/(1-k)
+		return Color.cmyk(f(r),f(g),f(b),k*MAX.k)
 		}
-
-	, toString = ( sMode, X, Y, Z, a )=>{
-		let s = X+','+Y+','+Z
-		return sMode + ( a!==null ? 'a('+s+','+a+')' : '('+s+')' )
+	// Output
+	, toString= (sMode,X,Y,Z,a)=>{
+		let s=X+','+Y+','+Z
+		return sMode+(a!==null?'a('+s+','+a+')':'('+s+')')
 		}
-	, value = ( n, sOption, sAxe )=>{
+	, value= (n,sOption,sAxe)=>{
 		return sOption
-			? Math.round( 100 * n/MAX[sAxe] ) +'%'
-			: Math.round( n )
+			?Math.round(100*n/MAX[sAxe])+'%'
+			:Math.round(n)
+		}
+	// ...
+	, aDEC='0,51,102,153,204,255'.split(',')
+	, contrast= (o1,o2)=>{
+		let _1=o1.toRGB(), _2=o2.toRGB()
+		, f= s => Math.abs(_1[s]-_2[s])
+		, brightness= o => (o.r*299+o.g*587+o.b*114)/1000
+		, difference= () => f('r')+f('g')+f('b')
+		return !(Math.abs(brightness(_1)-brightness(_2))<125||difference()<500)
 		}
 
-	// DIVERS
-	, aDEC = '0,51,102,153,204,255'.split(',')
-	, contrast =function( o1, o2 ){
-		let _1 = o1.toRGB(), _2 = o2.toRGB()
-		, f = s => Math.abs( _1[s] - _2[s] )
-		, brightness =  o => ( o.r*299 + o.g*587 + o.b*114 )/1000
-		, difference = () => f('r') + f('g') + f('b')
-		return !( Math.abs( brightness(_1) - brightness(_2) ) < 125 || difference() < 500 )
-		}
-	Object.assign( Color, {
-		hex :function( s ){
-			return Object.assign( isHEX( s ), {
+	return Object.assign( Color, {
+		hex:function( s ){
+			return Object.assign(isHEX( s ),{
 				mode: 'hex',
-				toString :function( m ){ return (m||'') + this },
+				toString :function( m ){return (m||'')+this},
 				toWeb :function(){return this.toHEX().toString('#')},
 				toHEX :function(){return this},
 				toRGB :function(){return HEXtoRGB(this)},
@@ -199,9 +192,9 @@ Color =(function(){
 				toCMYK :function(){return HEXtoRGB(this).toCMYK()}
 				})
 			},
-		rgb :function(R,G,B,A){
-			return Object.assign( is('rgba',R,G,B,A), {
-				toString :function( m ){ return toString('rgb', value(this.r,m,'r'), value(this.g,m,'g'), value(this.b,m,'b'), this.a ) },
+		rgb:function(R,G,B,A){
+			return Object.assign(is('rgba',R,G,B,A),{
+				toString :function( m ){ return toString('rgb',value(this.r,m,'r'),value(this.g,m,'g'),value(this.b,m,'b'),this.a)},
 				toWeb :function(){return this.toHEX().toString('#')},
 				toHEX :function(){return RGBtoHEX(this)},
 				toRGB :function(){return this},
@@ -210,9 +203,9 @@ Color =(function(){
 				toCMYK :function(){return RGBtoCMYK(this)}
 				})
 			},
-		hsv :function(H,S,V,A){
-			return Object.assign( is('hsva',H,S,V,A), {
-				toString :function( m ){ return toString('hsv', value(this.h||0), value(this.s,m,'s'), value(this.v,m,'v'), this.a ) },
+		hsv:function(H,S,V,A){
+			return Object.assign(is('hsva',H,S,V,A),{
+				toString :function( m ){ return toString('hsv',value(this.h||0),value(this.s,m,'s'),value(this.v,m,'v'),this.a)},
 				toWeb :function(){return this.toHEX().toString('#')},
 				toHEX :function(){return this.toRGB().toHEX()},
 				toRGB :function(){return HSVtoRGB(this)},
@@ -221,9 +214,9 @@ Color =(function(){
 				toCMYK :function(){return this.toRGB().toCMYK()}
 				})
 			},
-		hsl :function(H,S,L,A){
-			return Object.assign( is('hsla',H,S,L,A), {
-				toString :function( m ){ return toString('hsl', value(this.h||0), value(this.s,m,'s'), value(this.l,m,'l'), this.a ) },
+		hsl:function(H,S,L,A){
+			return Object.assign(is('hsla',H,S,L,A),{
+				toString :function( m ){ return toString('hsl',value(this.h||0),value(this.s,m,'s'),value(this.l,m,'l'),this.a)},
 				toWeb :function(){return this.toHEX().toString('#')},
 				toHEX :function(){return this.toRGB().toHEX()},
 				toRGB :function(){return HSLtoRGB(this)},
@@ -232,9 +225,9 @@ Color =(function(){
 				toCMYK :function(){return this.toRGB().toCMYK()}
 				})
 			},
-		cmyk :function(C,M,Y,K){
-			return Object.assign( is('cmyk',C,M,Y,K), {
-				toString :function( m ){ return 'cmyk('+ [ value(this.c,m,'c'), value(this.m,m,'m'), value(this.y,m,'y'), value(this.k,m,'k') ] +')' },
+		cmyk:function(C,M,Y,K){
+			return Object.assign(is('cmyk',C,M,Y,K),{
+				toString :function( m ){ return 'cmyk('+[value(this.c,m,'c'),value(this.m,m,'m'),value(this.y,m,'y'),value(this.k,m,'k')]+')' },
 				toWeb :function(){return this.toHEX().toString('#')},
 				toHEX :function(){return this.toRGB().toHEX()},
 				toRGB :function(){return CMYKtoRGB(this)},
@@ -243,32 +236,27 @@ Color =(function(){
 				toCMYK :function(){return this }
 				})
 			},
-		contrast :contrast,
-		getWebSafe :  m =>{
+		contrast: contrast,
+		getWebSafe: m =>{
 			let o = ( m.split ? Color( m ) : m ).toRGB()
-			, f = n => DECtoHEX( Math.round( n / 51 ) * 51 )
+			, f = n => DECtoHEX( Math.round(n/51)*51)
 			return f(o.r)+f(o.g)+f(o.b)
 			},
-		inRange : inRange,
-		visibleColor : o1 =>{
+		inRange: inRange,
+		visibleColor: o1 =>{
 			let n=aDEC.length, o2
 			for(let i=0;i<n;i++)for(let j=0;j<n;j++)for(let k=0;k<n;k++)
 				if( contrast( o1, o2 = Color.rgb( aDEC[i], aDEC[j], aDEC[k] )))
 					return o2.toWeb()
 			return '#000000'
 			},
-		visibleColors : o1 =>{
+		visibleColors: o1 =>{
 			let a=[]
-			, f = aDEC => {
-				let n=aDEC.length, o2
-				for(let i=0;i<n;i++)for(let j=0;j<n;j++)for(let k=0;k<n;k++)
-					if( contrast( o1, o2 = Color.rgb( aDEC[i], aDEC[j], aDEC[k] )))
-						a.push( o2.toWeb())
-				}
-			f( aDEC )
+			let n=aDEC.length, o2
+			for(let i=0;i<n;i++)for(let j=0;j<n;j++)for(let k=0;k<n;k++)
+				if( contrast( o1, o2 = Color.rgb( aDEC[i], aDEC[j], aDEC[k] )))
+					a.push( o2.toWeb())
 			return a
 			}
 		})
-
-	return Color
 	})()
--- js/unittest.js
+++ js/unittest.js
@@ -2,72 +2,82 @@ UnitTest =(function(){
 	let _ = ( sNodeName, sInnerHTML, sClassName ) =>{
 		let e = document.createElement( sNodeName )
 		if( sInnerHTML ) e.innerHTML = sInnerHTML
-		if( sClassName ) e.className = sClassName
+		if( sClassName ) e.className = 'UT_'+ sClassName
 		return e
 		}
-
 	class UnitTest {
 		constructor ( eTarget, a ){
-			eTarget.appendChild( this.evalList( a, 2 ))
+			this.nError = 0
+			let e = this.evalList( a, 2 )
+			eTarget.appendChild( e )
+			e.insertBefore(
+				this.nError
+					? _('DIV', 'Attention: '+this.nError+' erreur(s) !', 'error' )
+					: _('DIV', "Pas une erreur.", 'correct' )
+				, e.firstChild )
 			}
-		getTitle ( o, nLevel, eParent ){
-			return o.title
-				? eParent.appendChild( _('H'+ nLevel , o.title, 'title' ))
-				: null
-			}
-		getDesc ( o, eParent ){
-			return o.desc 
-				? eParent.appendChild( _('DIV', o.desc, 'desc' ) )
-				: null
-			}
-		getEval ( o, eParent ){
-			if( o.eval ){
-				let ePRE = _('PRE', o.eval, 'eval' )
-				try{
-					eval( o.eval )
-				}catch( e ){
-					bOK = false
-					let eDIV = _('DIV', e.toString(), 'error' )
-					ePRE.appendChild( eDIV )
-					// throw e
-					}
-				eParent.appendChild( ePRE )
-				}
-			}
-
 		evalList ( a, nLevel ){
 			let eDL = _('DL', null, 'list')
 			a.forEach( o =>{
 				let eDT = _('DT', null, 'section')
-				, bOK = true
-				, eTitle = this.getTitle( o, nLevel, eDT )
-				, eDesc = this.getDesc( o, eDT )
+				this.getTitle( o, nLevel, eDT )
+				this.getDesc( o, eDT )
 				this.getEval( o, eDT )
-
-				if( o.assertions ){
-					let eDL2 = _('DL', null, 'assertions' )
-					o.assertions.forEach( s =>{
-						let eDD = _('DD', '<code>'+ s +'</code>' )
-						try{
-							let b = eval( s )
-							bOK &&= b
-							eDD.className = b ? 'true' : 'false'
-						}catch( e ){
-							bOK = false
-							let eDIV = _('DIV', e.toString(), 'error' )
-							eDD.appendChild( eDIV )
-							// throw e
-							}
-						eDL2.appendChild( eDD )
-						eDT.appendChild( eDL2 )
-						})
-					}
-				if( o.list ){
-					eDT.appendChild( this.evalList( o.list, nLevel+1 ))
-					}
+				this.getAssertions( o, eDT )
+				this.getList( o, nLevel, eDT )
 				eDL.appendChild( eDT )
 				})
 			return eDL
+			}
+		getTitle ( o, nLevel, e ){
+			return o.title
+				? e.appendChild( _('H'+ nLevel , o.title, 'title' ))
+				: null
+			}
+		getDesc ( o, e ){
+			return o.desc 
+				? e.appendChild( _('DIV', o.desc, 'desc' ) )
+				: null
+			}
+		getEval ( o, e ){
+			if( o.eval ){
+				let ePRE = _('PRE', o.eval, 'eval' )
+				try{
+					eval( o.eval )
+				}catch( error ){
+					this.nError++
+					ePRE.appendChild( _('DIV', error.toString(), 'error' ))
+					// throw error
+					}
+				return  e.appendChild( ePRE )
+				}
+			return null
+			}
+		getAssertions ( o, e ){
+			if( o.assertions ){
+				let eDL2 = _('DL', null, 'assertions' )
+				o.assertions.forEach( s =>{
+					let eDD = _('DD', '<code>'+ s +'</code>' )
+					try{
+						let b = eval( s )
+						if( ! b ) this.nError++
+						eDD.className = b ? 'UT_true' : 'UT_false'
+					}catch( error ){
+						this.nError++
+						eDD.className ='UT_false'
+						eDD.appendChild( _('DIV', error.toString(), 'error' ))
+						// throw error
+						}
+					eDL2.appendChild( eDD )
+					return  e.appendChild( eDL2 )
+					})
+				}
+			return null
+			}
+		getList ( o, nLevel, e ){
+			return o.list
+				? e.appendChild( this.evalList( o.list, nLevel+1 ))
+				: null
 			}
 		}
 	return UnitTest
