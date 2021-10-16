b6a8f914c1d4841df898ffac485ad3dbfe971174
raphpell <pellicierraphael@neuf.fr>
2021-10-16 17:53:24 +0200

    redesign

--- src/Style.js
+++ src/Style.js
@@ -1,86 +1,69 @@
-// Dépendance : CssRules, Tag, Array.unique, each
+// Dépendance : CssRules
 
-Style ={
-	calculate( s, m ){
-		if( ! m && m !== 0 ) return ''
-		var bColor = /color/i.test( s ), sUnit = ''
-		switch( s ){
-			case 'font-size':
-			case 'height':
-			case 'width':
-				m=m<0?0:m
-				break
-			case 'opacity':
-				m = new Number ( m )
-				n = m <= 0 ? 0 :( m >= 1 ? 1 : m.toFixed( 2 ))
-				return 'opacity:'+ n
-			}
-		switch( s ){
-			// case 'font-size' : sUnit = 'em' ; break;
-			case 'font-weight' : sUnit = '' ; m = parseInt( m );break;
-			case 'background-position' : sUnit = '';break;
-			default :
-				sUnit = bColor ? '' : 'px'
-				if( sUnit == 'px' ) m = parseInt( m ) || 0
-			}
-		return s + ':' + m + sUnit + ';'
-		},
-	get( e, sAttr ){
-		let s = Style.getAttributeNS( sAttr )
-		let sValue = e.style[s]
-		|| e.currentStyle && e.currentStyle[s]
-		|| window.getComputedStyle && window.getComputedStyle( e )[s]
-		if( sValue && sValue!='0px' ) return sValue
-		if( window.CssRules ){
-			let f = sSelector =>{
-				let o = CssRules.get( sSelector )
-				let sValue = o && o[s]
-				return sValue && sValue!='0px' ? sValue : null
-				}
-			if( e.id ){
-				var m = f( '#'+e.id )
-				if( m ) return m
-				}
-			e.classList.forEach( sClassName =>{
-				sValue = f( '.'+sClassName ) || sValue
-				})
-			return sValue || null
-			}
-		},
-	getAttributeNS( s ){
-		if(~s.indexOf('-')) return s
-		let a=s.split('-')
-		for(let i=1,n=a.length;i<n;i++)
-			a[i] = a[i].charAt(0).toUpperCase()+a[i].substr(1)
-		return a.join('')
-		},
-	remove( m, s ){ // m == CssRule || Element
-		m=m.style
-		s=Style.getAttributeNS( s )
-		if( m && m[s]){
-			if( m.removeProperty ) m.removeProperty( s ) 
-				else try{ m[s] = '' }catch(e){}
-			}
-		},
-	set( m, s ){
-		var e = m
-		, m = m.style || m
-		, o = {}
-		, aAttr = []
-		each( ( m.cssText+';'+s ).split( ';' ),function(sAttr){
-			var a = sAttr.split( ':' ), key
-			if( a.length == 2 && a[1]){
-				aAttr.push( key = a[0].trim().toUpperCase())
-				o[ key ] = a[1] || ''
-				}
-			})
-		s = ''
-		each( Array.unique( aAttr ), function( key ){
-				s += key +':'+ o[ key ] +';'
-			})
-		var sBG1 = m.backgroundImage
-		m.cssText = s
-		if( sBG1 ) m.backgroundImage = sBG1 // For Chrome
-		return s
+Style =(()=>{
+	const reCssDecl = /\s*([a-z\-]+)\s*:\s*([^\;]+);\s*/g
+	, reNS = /-\w/g
+	, _o = {
+		'background-position':['null',''],
+		'font-size':['int0+','px'],
+		'font-weight':['int0+',''],
+		'height':['int0+','px'],
+		'opacity':['0..1',''],
+		'width':['int0+','px'],
 		}
-	}
+	, _F ={
+		'0+': n => n<=0?0:n,
+		'0..1': n => n<=0?0:(n>=1?1:n.toFixed(2)),
+		'int0+': n => n<=0?0:parseInt(n),
+		'null': n => n
+		}
+
+	return {
+		calculate( s, m ){
+			if(!m&&m!==0) return ''
+			if(_o[s]) return s+':'+_F[_o[s][0]](m)+_o[s][1]+";"
+			let sUnit = /color/i.test( s )?'':'px'
+			if(sUnit=='px') m=parseInt(m)||0
+			return s+':'+m+sUnit+";"
+			},
+		get( e, sAttr ){
+			let s = Style.getAttributeNS( sAttr )
+			let sValue = e.style[s]
+			|| e.currentStyle && e.currentStyle[s]
+			|| window.getComputedStyle && window.getComputedStyle( e )[s]
+			if( sValue && sValue!='0px' ) return sValue
+			if( window.CssRules ){
+				let f = sSelector =>{
+					let o = CssRules.get( sSelector )
+					let sValue = o && o[s]
+					return sValue && sValue!='0px' ? sValue : null
+					}
+				if( e.id ){
+					var m = f( '#'+e.id )
+					if( m ) return m
+					}
+				e.classList.forEach( sClassName =>{
+					sValue = f( '.'+sClassName ) || sValue
+					})
+				return sValue || null
+				}
+			},
+		getAttributeNS( s ){
+			if(!~s.indexOf('-')) return s
+			return s.replace( reNS, (s)=>s.charAt(1).toUpperCase())
+			},
+		remove( m, s ){ // m == CssRule || Element
+			m=m.style
+			s=Style.getAttributeNS(s)
+			if(m && m[s]){
+				if(m.removeProperty) m.removeProperty(s) 
+					else try{m[s]=''}catch(e){}
+				}
+			},
+		set( m, s ){
+			m=m.style||m
+			Array.from(s.matchAll(reCssDecl)).forEach(a=>m[a[1]]=a[2])
+			return m.cssText
+			}
+		}
+	})()
