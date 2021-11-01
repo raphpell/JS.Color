72e6420378820a86f94570d92db6fdb6a9a2ea7f
raphpell <pellicierraphael@neuf.fr>
2021-11-01 15:27:52 +0100

    Mouse update

--- src/Mouse.js
+++ src/Mouse.js
@@ -1,28 +1,44 @@
 Mouse =(function(){
-	const EventsSet=new Set(['mousedown','mouseup','mousemove','wheel'])
-	, getEvent=evt=>evt?evt:(window.event?window.event:null)
-	, a=['left','middle','right','back','forward']
+	// 'mousedown','mouseup','mousemove','wheel'
+	const getEvent=e=>e?e:(window.event?window.event:null)
+	, a=['Left','Middle','Right','Back','Forward']
 	, o={1:a[0],2:a[2],4:a[1],8:a[3],16:a[4]}
+	, _ ='mouse'
 	return {
-		button:function(evt){
-			return a[getEvent(evt).button]
+		button:function(e){
+			return _+a[getEvent(e).button]
 			},
-		buttons:function(evt){
-			let n=getEvent(evt).buttons||0
+		buttons:function(e){
+			let n=getEvent(e).buttons||0
 			,a=[]
-			,f=(nId)=>{if(n>=nId){a.push(o[nId]);n-=nId}}
+			,f=(nId)=>{if(n>=nId){a.push(_+o[nId]);n-=nId}}
 			f(16);f(8);f(4);f(2);f(1)
 			return a.join('+')
 			},
-		position:function(evt){
-			evt=getEvent(evt)
+		keys:function(e){
+			e=getEvent(e)
+			let a=[]
+			,f=s=>{if(e[s])a.push(s.substring(0,s.length-3).toUpperCase())}
+			f('ctrlKey');f('altKey');f('shiftKey');f('metaKey')
+			return a.join('+')
+			},
+		shortcut:function(e){
+			e=getEvent(e)
+			let a=[]
+			,f=s=>{if(s)a.push(s)}
+			f( Mouse.keys(e));f( Mouse.buttons(e));f( Mouse.wheel(e))
+			return a.join('+')
+			},
+		position:function(e){
+			e=getEvent(e)
 			return{ 
-				left:evt.pageX||evt.clientX||0,
-				top:evt.pageY||evt.clientY||0
+				left:e.pageX||e.clientX||0,
+				top:e.pageY||e.clientY||0
 				}
 			},
-		wheel:function(evt){
-			return getEvent(evt).deltaY>0?'down':'up'
+		wheel:function(e){
+			e=getEvent(e)
+			return e.deltaY===undefined?'':_+(e.deltaY>0?'Down':'Up')
 			}
 		}
 	})()
